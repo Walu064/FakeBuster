63741c221e33260378cc1cd828a65e0de15fadf2
Walu064 <waluwalu064@gmail.com>
2023-05-20 22:30:26 +0200

    keywords_filter with test.

--- samples/scripts/keywords_filter.py
+++ samples/scripts/keywords_filter.py
@@ -1,10 +1,18 @@
+import os
 import pandas
-from fuzzywuzzy import fuzz
+from fuzzywuzzy import fuzz 
 
-def keywords_filter(phrase : str, text : str):
-    similarity = fuzz.partial_ratio(phrase, text)
-    return similarity
+phrases_test = ["cryptocurrency", "bitcoin", "money", "investments"]
+path_test = os.path.join("..", "data", "learn_fake_data.csv")
+ads_frame = pandas.read_csv(path_test)
+ads_test = ads_frame['content']
 
-test = keywords_filter("Big tits", "Hot moms area ! Enlarge your penis !")
-print(test)
+def keywords_filter(phrases: list, ads_text: list):
+    query = " ".join(phrases)
+    for ad_text in ads_text:
+        similarity_value = fuzz.ratio(query, ad_text)
+        print("\nPHRASE: " + query)
+        print("AD_TEXT: " + ad_text)
+        print("Ad similar in " + str(similarity_value) + " percent.")
 
+keywords_filter(phrases_test, ads_test)
