435eeb50f81f50a41995511d957c195fb05d6062
Johny <johny.weee@protonmail.com>
2023-05-21 00:11:14 +0200

    first files and method

---
+++ samples/scripts/other_site_finder.py
@@ -0,0 +1,47 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Uruchom przeglądarkę i otwórz stronę
+options = webdriver.ChromeOptions()
+options.add_argument("--incognito")
+options.add_argument("--disable-extensions")
+options.add_argument("--enable-javascript")
+options.add_argument("--enable-images")
+options.add_argument("--disable-web-security")  # Wyłącz funkcje ochrony przeglądarki
+options.add_argument("--no-sandbox")
+options.add_argument("--ignore-certificate-errors")
+
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.onet.pl/')
+
+try:
+    time.sleep(20)
+    # Czekaj do 10 sekund na pojawienie się elementu na stronie
+    # divs = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div"))
+    # )
+    # print(divs)
+    # for div in divs:
+    #     if 'reklama' in div.text.lower():
+    #         print(f"Znaleziono div z id: {div.get_attribute('id')}")
+    #
+    # element = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.ID, 'adslot-top'))
+    # )
+
+    elements = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH, "//div[contains(., 'REKLAMA')]"))
+    )
+
+    for i, div in enumerate(elements):
+        if div.get_attribute('id'):
+            print(f"Znaleziono div z id: {div.get_attribute('id')}")
+            div.screenshot(f'element_{i}_screenshot.png')
+
+finally:
+    # Zamknij przeglądarkę
+    driver.quit()
