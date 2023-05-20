1b9a2e79f7c888f4b1f9659a17127f4b4fb3f87c
Johny <johny.weee@protonmail.com>
2023-05-20 21:47:33 +0200

    SVM method to predict worked

--- data_prep_test.py
+++ data_prep_test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from fakebuster.classification_model.SVM_pred import predict
 
 from samples.scripts.data_preparation import open_folder, prepare
 
@@ -6,9 +7,19 @@ path = "Reklamy_legit"
 
 if __name__ == "__main__":
 
-    paths_of_file = open_folder(path)
-    # print(paths_of_file)
+    '''Wywołanie przygotowania danych do nauki modelu
+        paths_of_file = open_folder(path)
+        # print(paths_of_file)
+    
+        df = prepare(paths_of_file)
+    
+        print(df)
+    '''
 
-    df = prepare(paths_of_file)
-
-    print(df)
+    '''przykład wywołania predict przyjmuje ramkę danych
+        text = pd.read_csv('E:\Hackathon\supervision2\FakeBuster\samples\data\learn_legit_data.csv')
+        text = text['content']
+        text = text[10:11]
+        x = predict(text)
+        print(str(x))
+    '''
