c933090b38e2eb56939dc7d76b9261566b618c7b
Walu064 <waluwalu064@gmail.com>
2023-05-20 13:13:06 +0200

    Text preprocessing implemented.

--- samples/scripts/fake_detector.py
+++ samples/scripts/fake_detector.py
@@ -5,17 +5,21 @@ import pytesseract
 from langdetect import detect
 from googletrans import Translator
 import nltk
-import nltk.corpus
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Ads detection Selenium (Czy tam coś), potem OpenCV i wycięcie posta z reklamą.
 
 # OCR: 
-image_path = 'obraz_mati.jpg'
-image = Image.open(image_path)
-text_from_ocr = pytesseract.image_to_string(image, lang="pol")
-print("Original text from OCR: ")
-print(text_from_ocr)
+# image_path = 'obraz_mati.jpg'
+# image = Image.open(image_path)
+# text_from_ocr = pytesseract.image_to_string(image, lang="pol")
+# print("Original text from OCR: ")
+# print(text_from_ocr)
 
+text_from_ocr = "Gorące Mamuśki w twojej okolicy! Powiększ penisa! +20cm w miesiąc!"
 # Language detection:
 detected_language = detect(text_from_ocr)
 
@@ -28,12 +32,23 @@ print("Detected language: "+detected_language)
 print("Translated text from OCR: ")
 print(translated_text_from_ocr)
 
-# Tu się zaczyna zabawa w NLP.
+# Stopword list download:
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
 
+# Funkcja preprocessingu tekstu
+def preprocess_text(text):
+    # Tokenizacja tekstu
+    tokens = word_tokenize(text)
+    # Usunięcie stopwords
+    tokens = [token for token in tokens if token.lower() not in stop_words]
+    # Lematyzacja
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    # Połączenie tokenów z powrotem w tekst
+    processed_text = ' '.join(tokens)
+    return processed_text
 
-##  To do:
-#   ads detection (Kręgiel)
-#   NLP (Walu, Góral)
-#   Classification (Jastrzymbie)
-#   Integration (Autisto)
-#   Trucie dupska mentorom, naruchiwanie danych do uczenia (Targos123)
+print(preprocess_text(translated_text_from_ocr))
