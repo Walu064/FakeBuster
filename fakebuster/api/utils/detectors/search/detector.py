1ef3ab696677753d16150b707a79660c801a49ac
Ma2000ciej <73307381+Ma2000ciej@users.noreply.github.com>
2023-05-21 03:20:35 +0200

    Please enter the commit message for your changes. Lines starting

--- fakebuster/api/utils/detectors/search/detector.py
+++ fakebuster/api/utils/detectors/search/detector.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 cwd = os.path.dirname(os.path.realpath(__file__))
 api_dir = os.path.dirname(os.path.dirname(os.path.dirname(cwd)))
 sys.path.append(api_dir)
@@ -24,7 +24,8 @@ options.add_experimental_option("detach", True)
 options.add_argument("--disable-notifications")
 options.add_argument("--disable-popup-blocking")
 options.add_argument("--start-maximized")
-stalaWalczakaWidht = 1.243333
+stalaWalczakaWidht = 1.243333                                                   ## This is smth like Planck's constant
+                                                                                ## needed to resize div from html source
 stalaWalczakaHeight = 1.54098
 
 
@@ -60,37 +61,33 @@ def bing_detect(data : SearchRequestModel) -> list[AdvertModel]:
     driver.save_screenshot("imageToCrop.png")
     AcceptCookies(driver)
     elements = driver.find_elements(By.XPATH, "//*[contains(text(), 'Reklama')]")
-    driver.save_screenshot("dupa.png")
-    print(len(elements))
+    driver.save_screenshot("imageToCrop.png")
     for i, element in enumerate(elements):
+        name = "Reklama_Nr_" + str(i) + ".png"
         parent = element.find_element(By.XPATH, ("./../../.."))
-        x = element.location["x"] + 15
-        y = element.location["y"] - 40
-        driver.save_screenshot("dupa.png")
+        left_border_to_crop = element.location["x"] + 15
+        top_border_to_crop = element.location["y"] - 40
+        driver.save_screenshot("imageToCrop.png")
         if i != 0:
             driver.execute_script("window.scrollTo(0, '%d');" % (parent.location['y'] - 100))
-            value = driver.execute_script("return window.pageYOffset;")
-            driver.save_screenshot("dupa.png")
-            y = 115
-        img = Image.open("dupa.png")
-        x1 = x + (parent.size['width'] * stalaWalczakaWidht)
-        y1 = y + (parent.size['height'] * stalaWalczakaHeight)
-        img = img.crop((x, y, x1, y1))  ##Left Top right bottom
-        img.save("Reklama_Nr_" + str(i) + ".png")
+            driver.save_screenshot("imageToCrop.png")
+            top_border_to_crop = 115
+        img = Image.open("imageToCrop.png")
+        right_border_to_crop = left_border_to_crop + (parent.size['width'] * stalaWalczakaWidht)
+        bottom_border_to_crop = top_border_to_crop + (parent.size['height'] * stalaWalczakaHeight)
+        img = img.crop((left_border_to_crop, top_border_to_crop, right_border_to_crop, bottom_border_to_crop))  ##Left Top right bottom
+        img.save(SCREENSHOTS_DIR + "\\" + name)
         img.close()
         parent = element.find_element(By.XPATH, ("./../../.."))
         childs = parent.find_elements(By.XPATH, (".//*"))
-        f = open("hfrefBing.txt", "a")
-        f.write(childs[1].get_property('href') + "\n")
-        f.close()
         list_to_return.append(AdvertModel(
-            url=href,
+            url=childs[1].get_property('href') + "\n",
             name="",
             destination_url=[],
             words=[],
             screenshot_ads=SCREENSHOTS_DIR + "\\" + name
         ))
-    return  list_to_return
+    return list_to_return
 
 
 
