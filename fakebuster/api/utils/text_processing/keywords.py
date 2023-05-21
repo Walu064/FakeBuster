596b8c483a90cb778f60deb7ca1724e324a399f6
Walu064 <waluwalu064@gmail.com>
2023-05-21 02:44:27 +0200

    keywords.py integrated with API

--- fakebuster/api/utils/text_processing/keywords.py
+++ fakebuster/api/utils/text_processing/keywords.py
@@ -1,17 +1,38 @@
+import os
+import pandas
+import spacy 
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+nlp = spacy.load('pl_core_news_lg')
+
+# Preprocessing requirements:
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+stop_words = set(stopwords.words())
+lemmatizer = WordNetLemmatizer()
+
 def filter_by_query(words : str, query : str) -> bool:
     query = query.split(" ")
     if len(query) == 0:
         return True
+    doc1 = nlp(str(words))
+    doc2 = nlp(str(query))
+    similarity_value = doc1.similarity(doc2) * 100
+    if similarity_value >= 30:
+        return True
+    else:
+        return False
 
-    # TODO: Keywords filtering
-    # Check if ad's keywords suit to query
-    
-    return True
 
-
-def get_keywords(text : str) -> list[str]:
-    keywords : list[str] = []
-    
-    # TODO: Get keywords from raw ad text
-    
+def get_keywords(text : str) -> str:
+    global stop_words, lemmatizer
+    tokens = word_tokenize(text)
+    tokens = [token for token in tokens if token.lower() not in stop_words]
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    keywords = ' '.join(tokens)
     return keywords
